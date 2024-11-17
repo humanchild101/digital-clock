a56cfda245042094c3047ec6fe38757b837cd91e
humanchild101 <nikhilamadhavi@gmail.com>
2024-11-17 12:53:22 -0500

    color change working

--- exp.py
+++ exp.py
@@ -15,6 +15,7 @@ white = (255, 255, 255, 255)
 gray_blue = (106, 129, 166, 255)
 navy_blue = (38, 66, 110, 255)
 midnight_purple = (37, 29, 112, 255)
+midnight_blue = (2, 15, 36, 255)
 wisteria = (135, 107, 219, 255)
 flowers = (219, 107, 189, 255)
 red = (255, 59, 75, 255)
@@ -24,6 +25,7 @@ olive = (121, 138, 83, 255)
 sea_glass = (143, 227, 182, 255)
 
 colors = [white, gray_blue, navy_blue, midnight_purple, wisteria, flowers, red, lava, lemonade, olive, sea_glass]
+i = 0 #for iterating through colors
 
 # the default ig
 win.fill(gray_blue)
@@ -41,37 +43,35 @@ def round_button(window, x, y, width, height, border_color, text, text_color, co
     # 1 rect. will be used to draw 2 rectangles --> 1 for border/one for fill if bg color provided
     button_rect = pygame.Rect((x, y, width, height))
 
+    #if a bg color was provided then do both the border rect and the button rect. but if not, just teh border rectangle
     if bg_color != (0, 0, 0, 0):
         pygame.draw.rect(window, bg_color, button_rect, 0, 10)
-        pygame.draw.rect(window, border_color, button_rect, 5, 10)
+        pygame.draw.rect(window, border_color, button_rect, 3, 10)
     else:
-        pygame.draw.rect(window, border_color, button_rect, 5, 10)
+        pygame.draw.rect(window, border_color, button_rect, 3, 10)
 
     w, h = font.size(text)
 
+    #words in the center of rect
     window.blit(words, (button_rect.centerx - w / 2, button_rect.centery - h / 2))
 
+    #if its clicked it should perform some function that is given as an argument
     if event.type == pygame.MOUSEBUTTONDOWN:
         if x + 10 <= mouse_pos[0] <= width + x + 10 and y + 10 <= mouse_pos[1] <= height + y + 10:
             command()
             pygame.display.update()
 
 
-def bg_change(color):
+#change to desired color. not in actual program.
+def manual_bg_change(color):
     win.fill(color)
 
-
-"""
-def color_drop():
-    round_button(10,50,175, 40, white, "White", white, lambda: bg_change(white))
-"""
-
-
-def happy():
-    win.fill(navy_blue)
+def bg_change_in_order():
+    global i
+    win.fill(colors[i])
+    i = (i+1) % len(colors)
 
 
-dropdown = pygame.Rect((10, 10, 200, 50))
 
 while True:
     for event in pygame.event.get():
@@ -79,7 +79,6 @@ while True:
             pygame.quit()
             sys.exit()
 
-    round_button(win, 10, 10, 175, 40, white, "Colors", white, lambda: bg_change(lemonade))
-    round_button(win, 10, 60, 175, 40, white, "Red", white, lambda: bg_change(red))
+    round_button(win, 10, 10, 175, 40, white, "Change Color", midnight_blue, bg_change_in_order)
 
     pygame.display.update()
