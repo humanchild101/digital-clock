246305a8a65a8b0c5050008b494ca1f87f15f057
humanchild101 <nikhilamadhavi@gmail.com>
2024-11-22 18:41:49 -0500

    made button into a class.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 import pygame
 from pygame import QUIT
 import sys
-
-# import time
+import datetime
+from Button import *
 
 pygame.init()
 
@@ -25,7 +25,7 @@ olive = (121, 138, 83, 255)
 sea_glass = (143, 227, 182, 255)
 
 colors = [white, gray_blue, navy_blue, midnight_purple, wisteria, flowers, red, lava, lemonade, olive, sea_glass]
-i = 0 #for iterating through colors
+i = 0  # for iterating through colors
 
 # the default ig
 win.fill(gray_blue)
@@ -33,44 +33,21 @@ win.fill(gray_blue)
 
 # round button function. (Surface, x_pos, y_pos, width, height, border_color, text you want, text_color,
 # button function, background color)
-def round_button(window, x, y, width, height, border_color, text, text_color, command, bg_color=(0, 0, 0, 0)):
-    mouse_pos = pygame.mouse.get_pos()
 
-    # font stuff for text
-    font = pygame.font.Font(None, 30)
-    words = font.render(text, False, text_color)
-
-    # 1 rect. will be used to draw 2 rectangles --> 1 for border/one for fill if bg color provided
-    button_rect = pygame.Rect((x, y, width, height))
-
-    #if a bg color was provided then do both the border rect and the button rect. but if not, just teh border rectangle
-    if bg_color != (0, 0, 0, 0):
-        pygame.draw.rect(window, bg_color, button_rect, 0, 10)
-        pygame.draw.rect(window, border_color, button_rect, 3, 10)
-    else:
-        pygame.draw.rect(window, border_color, button_rect, 3, 10)
-
-    w, h = font.size(text)
-
-    #words in the center of rect
-    window.blit(words, (button_rect.centerx - w / 2, button_rect.centery - h / 2))
-
-    #if its clicked it should perform some function that is given as an argument
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        if x + 10 <= mouse_pos[0] <= width + x + 10 and y + 10 <= mouse_pos[1] <= height + y + 10:
-            command()
-            pygame.display.update()
-
-
-#change to desired color. not in actual program.
+# change to desired color. not in actual program.
 def manual_bg_change(color):
     win.fill(color)
 
+
+# use list to change colors in order
 def bg_change_in_order():
     global i
     win.fill(colors[i])
-    i = (i+1) % len(colors)
+    i = (i + 1) % len(colors)
 
+
+def changing_time():
+    return
 
 
 while True:
@@ -79,6 +56,28 @@ while True:
             pygame.quit()
             sys.exit()
 
-    round_button(win, 10, 10, 175, 40, white, "Change Color", white, bg_change_in_order, midnight_blue)
+    button = Button(win, 10, 10, 175, 40, white, "Change Color", white, bg_change_in_order, midnight_blue)
+    button.draw()
+    button.clicked(event)
+
+    time = pygame.time.get_ticks()
+    # ok so create a new surface for this. put the datetime.time thing on it and update it every 1000 ms (so every second).. easy enough i hope its actually this simple.
+
+    now = datetime.datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+
+    time_surface = pygame.Surface((200, 100), pygame.SRCALPHA)
+
+    font1 = pygame.font.Font(None, 60)
+    time_text = font1.render(current_time, True, colors[3])
+
+    w, h = font1.size(current_time)
+
+    for ms in range(0, time, 1000):
+        time_text = font1.render(current_time, True, colors[3])
+        time_surface.blit(time_text, (200 - w / 2, 100 - h / 2))
+        win.blit(time_text, (400, 450))
+
+
 
     pygame.display.update()
